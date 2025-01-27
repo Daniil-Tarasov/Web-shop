91082cf712c02c3ff2e00e059684ea3dfee34a79
Daniil Tarasov <danya.tarasov.01@mail.ru>
2025-01-27 20:40:25 +0300

    start with models

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name_category = models.CharField(max_length=100, verbose_name='Наименование категории')
+    description = models.TextField(verbose_name='Описание категории')
+
+
+class Product(models.Model):
+    name_product = models.CharField(max_length=100, verbose_name='Наименование продукта')
+    description = models.TextField(verbose_name='Описание продукта')
+    image = models.ImageField(upload_to='images/', verbose_name='Изображение продукта')
+    category = models.ForeignKey(Category.name_category, on_delete=models.CASCADE)
+    price =  'f'
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
