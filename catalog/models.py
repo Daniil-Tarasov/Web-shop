5ba3dc40b0d2faa563e294baae93971851a835a2
Daniil Tarasov <danya.tarasov.01@mail.ru>
2025-01-29 19:11:35 +0300

    create models

--- catalog/models.py
+++ catalog/models.py
@@ -2,15 +2,31 @@ from django.db import models
 
 
 class Category(models.Model):
-    name_category = models.CharField(max_length=100, verbose_name='Наименование категории')
+    name_category = models.CharField(max_length=150, verbose_name='Наименование категории')
     description = models.TextField(verbose_name='Описание категории')
+
+    def __str__(self):
+        return f'{self.name_category}: {self.description}'
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['name_product']
 
 
 class Product(models.Model):
-    name_product = models.CharField(max_length=100, verbose_name='Наименование продукта')
+    name_product = models.CharField(max_length=150, verbose_name='Наименование продукта')
     description = models.TextField(verbose_name='Описание продукта')
     image = models.ImageField(upload_to='images/', verbose_name='Изображение продукта')
-    category = models.ForeignKey(Category.name_category, on_delete=models.CASCADE)
-    price =  'f'
-    created_at = models.DateField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория продукта', related_name="product")
+    price =  models.IntegerField(verbose_name='Цена продукта')
+    created_at = models.DateField(auto_now_add=True, verbose_name='Дата создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата последнего изменения')
+
+    def __str__(self):
+        return f'{self.name_product} - {self.category}. Описание: {self.description}. Цена: {self.price}'
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+        ordering = ['name_product']
