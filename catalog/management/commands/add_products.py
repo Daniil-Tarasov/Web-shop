818ee309324eb56ba5d0208cd05f7dc375090427
Daniil Tarasov <danya.tarasov.01@mail.ru>
2025-01-30 21:03:07 +0300

    create commands with fixtures

--- catalog/management/commands/add_products.py
+++ catalog/management/commands/add_products.py
@@ -1,4 +1,5 @@
 from django.core.management.base import BaseCommand
+from django.core.management import call_command
 from catalog.models import Category, Product
 
 class Command(BaseCommand):
@@ -9,32 +10,7 @@ class Command(BaseCommand):
         Category.objects.all().delete()
         Product.objects.all().delete()
 
-        category, _ = Category.objects.get_or_create(name_category='Безопасность', description='Защита от вирусов, взлома и тд.')
-
-        products = [
-            {
-                'name_product': 'Касперский Premium',
-                'description': 'Лучший антивирус на рынке на год',
-                'category': category,
-                'price': 22
-            },
-            {
-                'name_product': 'Касперский Plus',
-                'description': 'Лучший антивирус на рынке на год',
-                'category': category,
-                'price': 21
-            },
-            {
-                'name_product': 'Касперский Standard',
-                'description': 'Лучший антивирус на рынке на год',
-                'category': category,
-                'price': 17
-            }
-        ]
-
-        for product_data in products:
-            product, created = Product.objects.get_or_create(**product_data)
-            if created:
-                self.stdout.write(self.style.SUCCESS(f'Successfully added product: {product.name_product}'))
-            else:
-                self.stdout.write(self.style.WARNING(f'Product already exist: {product.name_product}'))
+        categories = call_command('loaddata', 'category_fixture.json')
+        self.stdout.write(self.style.SUCCESS('Successfully loaded data from fixture'))
+        products = call_command('loaddata', 'product_fixture.json')
+        self.stdout.write(self.style.SUCCESS('Successfully loaded data from fixture'))
