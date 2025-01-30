e0b1789ab6a77b40daa0d787e4684592ff682aa6
Daniil Tarasov <danya.tarasov.01@mail.ru>
2025-01-30 18:42:14 +0300

    fxs models

--- catalog/models.py
+++ catalog/models.py
@@ -17,8 +17,8 @@ class Category(models.Model):
 class Product(models.Model):
     name_product = models.CharField(max_length=150, verbose_name='Наименование продукта')
     description = models.TextField(verbose_name='Описание продукта')
-    image = models.ImageField(upload_to='images/', verbose_name='Изображение продукта')
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория продукта', related_name="product")
+    image = models.ImageField(upload_to='images/', verbose_name='Изображение продукта', blank=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, verbose_name='Категория продукта', related_name="product", null=True)
     price =  models.IntegerField(verbose_name='Цена продукта')
     created_at = models.DateField(auto_now_add=True, verbose_name='Дата создания')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата последнего изменения')
