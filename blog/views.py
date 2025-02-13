2f14db214517248834f6cb27e0a31789b22d7fa6
Daniil Tarasov <danya.tarasov.01@mail.ru>
2025-02-13 18:20:57 +0300

    end main task

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,6 @@
 from django.forms import ModelForm
-from django.shortcuts import render
-from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView, DetailView
+from django.urls import reverse_lazy, reverse
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 
 from blog.models import Blog
 
@@ -9,15 +8,23 @@ from blog.models import Blog
 class BlogsListView(ListView):
     model = Blog
 
+    def get_queryset(self):
+        return Blog.objects.filter(flag_publication=True)
 
 class BlogDetailView(DetailView):
     model = Blog
+
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.counter_views += 1
+        self.object.save()
+        return self.object
 
 
 class BlogForm(ModelForm):
     class Meta:
         model = Blog
-        fields = {"title", "content", "preview"}
+        fields = {"title", "content", "preview", "flag_publication"}
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -40,4 +47,19 @@ class BlogForm(ModelForm):
 class BlogCreateView(CreateView):
     template_name = 'blog/blog_form.html'
     form_class = BlogForm
+    success_url = reverse_lazy('blog:blogs_list')
+
+
+class BlogUpdateView(UpdateView):
+    model = Blog
+    form_class = BlogForm
+    template_name = 'blog/blog_form.html'
+    success_url = reverse_lazy('blog:blogs_list')
+
+    def get_success_url(self):
+        return reverse('blog:blog_detail', args=[self.kwargs.get('pk')])
+
+
+class BlogDeleteView(DeleteView):
+    model = Blog
     success_url = reverse_lazy('blog:blogs_list')
