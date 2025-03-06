6bc834078240189a985110d3cec4e95699e7cfc3
Daniil Tarasov <danya.tarasov.01@mail.ru>
2025-03-06 20:00:27 +0300

    Task with *

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,5 @@
-from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from django.forms import ModelForm
 
 from users.models import User
 
@@ -11,3 +11,42 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ("email", "password1", "password2")
+
+
+class UserForm(ModelForm):
+    class Meta:
+        model = User
+        fields = ('avatar', 'email', 'first_name', 'last_name', 'phone_number', 'country')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields["email"].widget.attrs.update({
+            'placeholder': 'Введите ваш email',
+            'class': "form-control"
+        })
+
+        self.fields["first_name"].widget.attrs.update({
+            'placeholder': "Укажите ваше имя",
+            'class': "form-control"
+        })
+
+        self.fields["last_name"].widget.attrs.update({
+            'placeholder': "Укажите вашу фамилию",
+            'class': "form-control"
+        })
+
+        self.fields["phone_number"].widget.attrs.update({
+            'placeholder': "Напишите свой номер телефона",
+            'class': "form-control"
+        })
+
+        self.fields["country"].widget.attrs.update({
+            'placeholder': "Укажите страну",
+            'class': "form-control"
+        })
+
+        self.fields["avatar"].widget.attrs.update({
+            'class': "form-control"
+        })
+
--- users/views.py
+++ users/views.py
@@ -1,9 +1,9 @@
 from django.core.mail import send_mail
-from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.urls import reverse_lazy, reverse
+from django.views.generic import CreateView, DetailView, UpdateView
 
 from config.settings import EMAIL_HOST_USER
-from users.forms import CustomUserCreationForm
+from users.forms import CustomUserCreationForm, UserForm
 from users.models import User
 
 
@@ -24,3 +24,17 @@ class RegisterView(CreateView):
         from_email = EMAIL_HOST_USER
         recipient_list = [user_email]
         send_mail(subject, message, from_email, recipient_list)
+
+
+class UserDetailView(DetailView):
+    model = User
+
+
+class UserUpdateView(UpdateView):
+    model = User
+    template_name = 'users/user_form.html'
+    form_class = UserForm
+    success_url = reverse_lazy('catalog:home')
+
+    def get_success_url(self):
+        return reverse('users:user_detail', args=[self.kwargs.get('pk')])
